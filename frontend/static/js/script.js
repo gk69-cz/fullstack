61f5ff09d0b5186930db51c69f3b42c2c6778841
Christy Zachariah <zachariahchristy@gmail.com>
2024-11-23 16:23:02 +0530

    restart

--- frontend/static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('fetch-button').addEventListener('click', () => {
-    fetch('/api/hello')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('api-message').textContent = data.message;
-        })
-        .catch(error => console.error('Error:', error));
-});
